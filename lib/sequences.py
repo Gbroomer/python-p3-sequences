25d70690c1709afd7492eb4ed65dd871a3a81fcc
Grant Bruemmer <grantbruemmer@gmail.com>
2023-07-25 10:26:57 -0500

    I hate when labs work fine but dont pass :)

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    fibonacci_seq = []
+    if length > 0:
+        
+        fibonacci_seq = [0, 1]
+    
+        while len(fibonacci_seq) < length:
+            next_term = fibonacci_seq[-1] + fibonacci_seq[-2]
+            fibonacci_seq.append(next_term)
+        
+        return print(fibonacci_seq)
+    else:
+        return print(fibonacci_seq)
+        
+        
+
+print_fibonacci(15)
